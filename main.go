c508f0c1a79c33638e213036dba61a62b5afeca1
Laura <milanbartky@gmail.com>
2024-03-05 16:03:01 +0100

    small fix

--- main.go
+++ main.go
@@ -18,6 +18,8 @@ func main() {
 	pwd, err := os.Getwd()
 	must(err)
 
+	port := parsePort()
+
 	gin.SetMode(gin.ReleaseMode)
 
 	r := gin.New()
@@ -32,16 +34,16 @@ func main() {
 	// Handle static files
 	r.Use(HandleBasic(pwd))
 
-	InfoPlain("Host: ", "http://localhost")
+	InfoPlain("Host: ", "http://localhost"+port)
 	InfoPlain("Root: ", pwd)
 	fmt.Println()
 
-	exec.Command("rundll32", "url.dll,FileProtocolHandler", "http://localhost").Start()
+	exec.Command("rundll32", "url.dll,FileProtocolHandler", "http://localhost"+port).Start()
 
-	must(r.Run(port()))
+	must(r.Run(port))
 }
 
-func port() string {
+func parsePort() string {
 	if len(os.Args) > 1 {
 		num, err := strconv.ParseInt(os.Args[1], 10, 64)
 		must(err)
